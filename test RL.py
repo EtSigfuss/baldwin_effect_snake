54e865a81fd3a11b5d71dd6bd972baea080eceba
EtSigfuss <etigfuss@gmail.com>
2026-02-28 21:34:11 -0700

    small changes for testing rl

--- test RL.py
+++ test RL.py
@@ -6,30 +6,35 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from agent_interface import featurize_state
+from constants import ACTION_STRING, ACTIONS, SNAKE_FEATURE_COLS
 from snake import Snake
-from solve_snake import featurize_state
 from torch_logic import Agent
 
 #enviroment parames
-height = 30
-width = 30
+height = 10
+width = 10
 lifespan = 5000
-frame_rate = 500
+frame_rate = 200
 
-# GA hyperparams
-pop = 50
-generations = 50
-mutation_rate = 0.10
-mutation_scale = 0.4
 
 #Torch params
+input_size = len(SNAKE_FEATURE_COLS)
+hidden_size = 2**3
+output_size = len(ACTIONS)
+starting_epsilon = 1
 
 
 seed_value = 1234
 rng = np.random.default_rng(seed_value)#global rng
 
 
-agent = Agent(episodes= 500)
+agent = Agent(
+                        input_size=input_size,
+                        hidden_size=hidden_size,
+                        output_size = output_size,
+                        decay_actions= 30000,
+                        epsilon=starting_epsilon,)
 env = Snake(
         width=width,
         height=height,
@@ -46,6 +51,7 @@ total_reward = 0.0
 
 #test function that you ctrl c out of, infinitely trains a snake agent with 254 hidden layers
 while True:
+    state = env.reset()
     while True:
         features = featurize_state(state)
         action = agent.get_action(features)
@@ -55,10 +61,8 @@ while True:
 
         agent.train(old_state=state, action=action, reward=reward, new_state=new_state, done=done)
 
-        env.render()
+        env.render(epsilon=agent.epsilon)
 
         state = new_state
         if done:
             break
-    state = env.reset()
-    
